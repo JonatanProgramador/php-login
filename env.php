a3f4d5a94bd469331c8f2e1585c3b7a0c5730198
Jonatán Molina Rodríguez <battusayprogramador@gmail.com>
2024-09-10 18:29:20 +0200

    Update env.php

--- env.php
+++ env.php
@@ -1,12 +1,1 @@
-<?php
-const DB_HOST = "d64951fe163d";
-const DB_NAME = "prueba";
-const DB_USER = "root";
-const DB_PASSWORD = "1234";
 
-const EMAIL_HOST = "smtp.gmail.com";
-const EMAIL_NAME = "battusayprogramador@gmail.com";
-const EMAIL_PASSWORD = "fxex khru facs nchn";
-const EMAIL_PORT = 587;
-
-const RUTA = "/var/www/html/login/";
