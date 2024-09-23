7708d2405469635066df94fb25516b3bdf52e899
Jonatan Molina <battusayprogramador@gmail.com>
2024-09-23 19:58:06 +0200

    Cambios en las semillas de usuarios
    Se ha cambiado información sensible de las semillas de usuarios.

--- app/database/seeds/UserSeed.php
+++ app/database/seeds/UserSeed.php
@@ -2,10 +2,10 @@
 require_once RUTA. "app/models/UserModel.php";
 
 const USERDATASEED = [
-    ["name"=>"battusay", "password"=>"1234", "message"=>"el poderoso", "email"=>"battusay000@gmail.com", "emailConfirm"=>"TRUE"],
-    ["name"=>"gines", "password"=>"5678", "message"=>"el cabezon", "email"=>"gines@gmail.com"],
-    ["name"=>"yeff", "password"=>"asdf", "message"=>"el manco", "email"=>"yes@gmail.com"],
-    ["name"=>"raven", "password"=>"qwer", "message"=>"el mariquita", "email"=>"raven@gmail.com"]
+    ["name"=>"battusay", "password"=>"1234", "message"=>"el mago", "email"=>"battusay@gmail.com", "emailConfirm"=>"TRUE"],
+    ["name"=>"gines", "password"=>"5678", "message"=>"el guerrero", "email"=>"gines@gmail.com"],
+    ["name"=>"yeff", "password"=>"asdf", "message"=>"el guardian", "email"=>"yes@gmail.com"],
+    ["name"=>"raven", "password"=>"qwer", "message"=>"el sacerdote", "email"=>"raven@gmail.com"]
 ];
 
  class UserSeed {
